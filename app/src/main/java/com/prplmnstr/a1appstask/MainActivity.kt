598a04b1479e368be77dc88cc2dca5208a0e3de8
Santosh <santoshinchalakaranje@gmail.com>
2024-03-13 20:47:01 +0530

    Initial Commit - project setup

---
+++ app/src/main/java/com/prplmnstr/a1appstask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.prplmnstr.a1appstask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
